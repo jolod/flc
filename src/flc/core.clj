110f11dfd6eec3bff699f4bd81a5507570ab234b
Johan Lodin <jolod@users.noreply.github.com>
2020-05-27 01:04:06 +0200

    Add `start!*`, `map-programs`, and `map-programs+` to flc.core.

--- src/flc/core.clj
+++ src/flc/core.clj
@@ -1,47 +1,56 @@
 (ns flc.core
   (:require [flc.let-like :as let-like]
-            [flc.process :as process :refer [process]]
+            [flc.process :as process]
             [flc.program :as program]
-            [flc.component :as component :refer [component]]
+            [flc.component :as component]
             [flc.map-like :as m]))
 
 (defn constants
-  "Takes a map-like sequence of names and values, and returns a map-like sequence of components where the programs has the values as states."
+  "Takes a map-like sequence of names and values, and returns a map-like sequence of components where the programs have the values as states."
   [m]
-  (m/fmap #(component (program/constant %))
+  (m/fmap #(component/component (program/constant %))
           m))
 
-(defn -processify
-  "Takes a sequence of computations on the form [name [f names]] and updates the function to extract the state from all arguments.
+(defn map-programs
+  "Rewrites the components so that the program is replaced by the return value of `f`, which takes the program as the argument."
+  [f components]
+  (m/fmap #(component/update-program % f) components))
 
-  Subject to name change."
-  [components]
-  (m/fmap (fn [[f deps]]
-            [(fn [& args]
-               (apply f (map process/state args)))
-             deps])
-          components))
-
-(defn -map-state-from-args [component]
-  (component/update-program component
-                            #(fn [& args]
-                               (apply % (map process/state args)))))
+(defn map-programs+
+  "Like `update-programs` where each program is replaced by the return value of `f`, but `f` takes the name and the component instead of the program. The program is accessed via `flc.component/program` instead. This is useful when you need the name and/or the dependencies of a program to produce the new program."
+  [f components]
+  (m/fmap-keyed (fn [name component]
+                  (component/update-program component (constantly (f name component))))
+                components))
 
 (defn ->let-like
   "Turns a sequence of components into a sequence of computations that works with e.g. `arrange` and `run` from `flc.let-like`."
   [components]
   (->> components
-       (m/fmap -map-state-from-args)
+       (map-programs #(fn [& args]
+                        (apply % (map process/state args))))
        (m/fmap component/->let-like)))
 
-(defn start! [components]
+(defn start!*
+  "Like `start!`, but does not arrange the components first, and thus support name shadowing."
+  [components]
+  (->> components
+       ->let-like
+       let-like/run
+       rseq))
+
+(defn start!
+  "Arrange the map-like sequence of named components in dependency order (if necessary) and then runs each program with the arguments as specified by its dependencies. Returns a map-like sequence of named processes, in reverse start order."
+  [components]
   (->> components
        ->let-like
        let-like/arrange
        let-like/run
        rseq))
 
-(defn arrange [components]
+(defn arrange
+  "Arranged the map-like sequence of components in dependency order, such that the first component has no dependencies and the second one can make use of the first, etc."
+  [components]
   (->> components
        (m/fmap (juxt identity component/dependencies))
        let-like/arrange
